642e7ff591c1438f0e4d93a2aa3e87196cfc4816
robertsbd <roberts.b.d@gmail.com>
2017-05-07 18:27:43 +0100

    update

---
+++ Playing.hs
@@ -0,0 +1,21 @@
+module FirstScript where
+
+    size :: Integer
+    size = 12+13
+
+    square :: Integer -> Integer
+    square n = n*n
+
+    double :: Integer -> Integer
+    double n = 2*n
+
+    double_and_square :: Integer -> Integer
+    double_and_square x = square (double x) 
+
+    square_and_double :: Integer -> Integer
+    square_and_double x = double (square x)      
+
+    example :: Integer
+    example = double (size - square(2*2))
+
+    
